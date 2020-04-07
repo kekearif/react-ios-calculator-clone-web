4b9fb0326b9720dc09476e776110f1402fce5d7c
keke Arif <kekearif@googlemail.com>
2020-04-07 20:56:16 +0800

    Updates

---
+++ precache-manifest.5af3db323f440c0f8015595308664f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6df79259deb43a99bef76d7676726e6",
+    "url": "/react-ios-calculator-clone-web/index.html"
+  },
+  {
+    "revision": "ee220681df57f5c23e1c",
+    "url": "/react-ios-calculator-clone-web/static/css/main.eb6a463d.chunk.css"
+  },
+  {
+    "revision": "071589bcd8691238d3c5",
+    "url": "/react-ios-calculator-clone-web/static/js/2.d087b8bb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-ios-calculator-clone-web/static/js/2.d087b8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee220681df57f5c23e1c",
+    "url": "/react-ios-calculator-clone-web/static/js/main.7e780a6d.chunk.js"
+  },
+  {
+    "revision": "c3d74796fd7538faeb00",
+    "url": "/react-ios-calculator-clone-web/static/js/runtime-main.bb476f05.js"
+  }
+]);
