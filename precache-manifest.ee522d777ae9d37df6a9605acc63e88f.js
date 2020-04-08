1a535f74b6756fef70a3724b8212fe3988530c14
keke Arif <kekearif@googlemail.com>
2020-04-08 22:41:12 +0800

    Updates

--- precache-manifest.ee522d777ae9d37df6a9605acc63e88f.js
+++ precache-manifest.ee522d777ae9d37df6a9605acc63e88f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f946a9c217e60a015a1f810b8d8934f",
+    "revision": "232c2d795b2cb5acd21ce0acfcd64e1c",
     "url": "/react-ios-calculator-clone-web/index.html"
   },
   {
-    "revision": "90d3bb637eca9691fb1e",
-    "url": "/react-ios-calculator-clone-web/static/css/main.1435d8a6.chunk.css"
+    "revision": "4163df69afb7de3bbdec",
+    "url": "/react-ios-calculator-clone-web/static/css/main.39a88f1e.chunk.css"
   },
   {
     "revision": "071589bcd8691238d3c5",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-ios-calculator-clone-web/static/js/2.d087b8bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90d3bb637eca9691fb1e",
-    "url": "/react-ios-calculator-clone-web/static/js/main.86bf3d49.chunk.js"
+    "revision": "4163df69afb7de3bbdec",
+    "url": "/react-ios-calculator-clone-web/static/js/main.cb9f7410.chunk.js"
   },
   {
     "revision": "c3d74796fd7538faeb00",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ios-calculator-clone-web/precache-manifest.21c018db25bc7d364b1bc30de99fe2af.js"
+  "/react-ios-calculator-clone-web/precache-manifest.ee522d777ae9d37df6a9605acc63e88f.js"
 );
 
 self.addEventListener('message', (event) => {
